7d90ceec27229cd950737b22acfda801f6d10863
L. F. Pereira <luisfgper@gmail.com>
2023-06-10 16:17:40 +0200

    Make day01 code more rustacean

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -3,40 +3,25 @@ use std::fs;
 fn parse(file_path: &str) -> Vec<Vec<usize>> {
     let contents = fs::read_to_string(file_path).unwrap();
 
-    let mut elves_calories: Vec<Vec<usize>> = Vec::new();
-    elves_calories.push(Vec::new());
-
-    for line in contents.lines() {
-        match line {
-            "" => elves_calories.push(Vec::new()),
-            _ => {
-                let calories: usize = line.parse().unwrap();
-                elves_calories.last_mut().unwrap().push(calories);
-            }
-        };
-    }
-    elves_calories
+    contents
+        .split("\n\n")
+        .map(|x| {
+            x.lines()
+                .map(|y| y.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
 }
 
-fn _sum_calories(elves_calories: &Vec<Vec<usize>>) -> Vec<usize> {
-    let elves_sum: Vec<usize> = elves_calories.iter().map(|x| x.iter().sum()).collect();
-
-    elves_sum
-}
-
-fn sum_calories(elves_calories: &Vec<Vec<usize>>) -> Vec<usize> {
-    let mut elves_sum: Vec<usize> = Vec::new();
-    for elf_calories in elves_calories.iter() {
-        elves_sum.push(elf_calories.iter().sum::<usize>());
-    }
-
-    elves_sum
+fn sum_calories(elves_calories: &[Vec<usize>]) -> Vec<usize> {
+    elves_calories.iter().map(|x| x.iter().sum()).collect()
 }
 
 pub fn solve_part1(file_path: &str) -> usize {
     let elves_calories = parse(file_path);
 
     let elves_sum = sum_calories(&elves_calories);
+
     let max = elves_sum.iter().max().unwrap();
 
     *max
